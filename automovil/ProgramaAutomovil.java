95e8413c08c7a7080cd3eb9a7fd0c8992556da45
richardjrv <richard.ruizvuni@gmail.com>
2025-06-02 15:08:26 -0500

    Add files via upload

---
+++ automovil/ProgramaAutomovil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa.automovil;
+
+/**
+ *
+ * @author Janeth Ruiz
+ */
+public class ProgramaAutomovil {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
